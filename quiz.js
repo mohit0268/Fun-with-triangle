d62be7838e5535d360c8451d5cdd0165a5bda6a3
mohit0268 <mohit0268@users.noreply.github.com>
2021-09-10 16:49:24 +0530

    quiz completed

--- quiz.js
+++ quiz.js
@@ -1,3 +1,21 @@
 const quizForm=document.querySelector(".form-quiz")
 const QuizBtn=document.querySelector("#submit-btn");
-const Output=document.querySelector("#output");
+const QuizOutput=document.querySelector("#output");
+
+const correctAnswer=["90°","Right angled"]
+
+function calculateScore(){
+    var index=0;
+    var score=0;
+    const formResults=new FormData(quizForm);
+    for(let value of formResults.values()){
+        if(value===correctAnswer[index]){
+            score =score +1;
+        }
+        index= index+1;
+    }
+    QuizOutput.innerText="Your score is " + score;
+}
+
+
+QuizBtn.addEventListener("click",calculateScore);
