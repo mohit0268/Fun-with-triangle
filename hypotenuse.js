8ed0d806295faea64645ea99672aff8f83104a0d
mohit0268 <mohit0268@users.noreply.github.com>
2021-09-14 16:59:51 +0530

    input processing output of hypotenuse

---
+++ hypotenuse.js
@@ -0,0 +1,20 @@
+const inputSide=document.querySelectorAll(".input-side");
+const HypoBtn=document.querySelector("#hypo-btn");
+const Output=document.querySelector(".hypo-output");
+
+
+function calculateSquare(i,j){
+const sumOfSquares=i*i + j*j;
+
+return sumOfSquares;
+}
+
+function calculateHypotenuse(){
+    const sumOfSquares=calculateSquare(Number(inputSide[0].value),Number(inputSide[1].value));
+    const lengthofSquares=Math.sqrt(sumOfSquares);
+    // console.log(length);
+    Output.innerText="The length of hypotenuse is "+lengthofSquares;
+}
+
+
+HypoBtn.addEventListener("click",calculateHypotenuse);
